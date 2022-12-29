a2d38e72290c57e69d1b3400164b9958d4f83c67
José Andrés Ríos Hidalgo <78713642+jose2899@users.noreply.github.com>
2022-12-29 15:33:27 -0500

    Update buid.sh

--- buid.sh
+++ buid.sh
@@ -7,5 +7,4 @@ pip install --upgrade pip
 pip install --force-reinstall -U setuptools
 pip install --upgrade pippip install --force-reinstall -U setuptools
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
 python manage.py migrate
